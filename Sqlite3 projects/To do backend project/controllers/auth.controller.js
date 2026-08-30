b6f441f756d223e896225d3dce803c60d41e0db8
Guru Kiran KM <kmgurukiran01@gmail.com>
2026-08-30 11:40:19 +0530

    auth.controller is done

---
+++ Sqlite3 projects/To do backend project/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+const bcrypt=require('bcrypt')
+const generationToken=require('../utils/jwt');
+const { use } = require('react');
+const generateToken = require('../utils/jwt');
+
+const registeUser=async (req,res)=>{
+    const {username, password, name}=req.body
+
+    
+if(!username || !password|| !name){
+    return res.status(400).json({
+        message:"Username, password and name are required"
+    })
+}
+try{
+    const user=await req.app.locals.db.get(`select * from users where username = ?`,[username]);
+    if(user){
+        return res.status(400).json({
+            message:"username already exists"
+        })
+    }
+    const hashedpassword= await bcrypt.hash(password,10) 
+    const  result=await req.app.locals.db.run(
+        `INSERT INTO users (username, password, name)values (?, ?, ?)`,[username,hashedpassword,name]
+    );
+    res.status(201).json({
+        message:'user registered successfully',
+    userId:result.lastId,
+})
+
+}catch(error){
+  res.status(500).json({
+    message:"internal server error",
+  });
+ }
+}
+
+const loginuser=async(req,res)=>{
+ const {username,password}=req.body
+ if(!username || !password){
+    return res.status(400).json({
+        message:"username and password are required",
+    })
+ }
+
+ try{
+   const user=await req.app.locals.db.get(`select * from users where username=?`,[username])
+   if(!user){
+    return res.status(400).json({
+        message:"invalid username or password",
+    })
+   }
+   const ispasswordmatch=await bcrypt.compare(password,user.password)
+   if(!ispasswordmatch){
+    return res.status(400).json({
+        message:"Invalid password",
+    })
+   }
+
+   const token=generateToken(user.id)
+
+   res.json({
+    message:"login successfully!",
+    token:token,
+   });
+ }catch(error){
+      res.status(500).json({
+    message:"internal server error",
+  });
+ }
+
+ 
+}
+
+
+module.exports={
+    registeUser,loginuser
+}
--- Sqlite3 projects/To do backend project/middleware/auth.js
+++ Sqlite3 projects/To do backend project/middleware/auth.js
@@ -1,2 +1,28 @@
 const jwt= require('jsonwebtoken')
 
+const authentication=(req,res,next)=>{
+    const author=req.header['authorization'];
+    if(!author){
+       return res.status(401).json({
+        message:"Authorization header missing",
+       });
+    }
+    const jwtToken=author.split(' ')[1]
+    if(!jwtToken){
+         return res.status(401).json({
+            message:'Token missing',
+         });
+    }
+
+    
+    jwt.verify(jwtToken, process.env.JWT_SECRET,(error,decoded)=>{
+        if(error){
+            return res.status(401).json({
+                message:"invalid or expired token"
+            })
+        }
+        req.userId=decoded.userId;
+
+        next();
+    })
+}
